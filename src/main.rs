64a540af1549bbcb9eea4ab9a7a353c765ea9f01
Rohan Sharma <sharma.rohan07@proton.me>
2026-03-31 23:14:15 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::{TcpListener, TcpStream}};
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,7 +12,18 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap();
+                // _stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buffer = [0; 4096];
+                loop {
+                    let n = _stream.read(&mut buffer).unwrap();
+                    if n == 0 {
+                        // EEEEEEEEEEEEEEEEEEEEEEEE
+                        break;
+                    }
+                    if buffer.starts_with(b"*1\r\n$4\r\nPING\r\n")  {
+                         _stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
